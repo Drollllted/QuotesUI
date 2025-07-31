06ac1a5314eb1689a5de0f21052bb49b1b07eff8
Artem Tarasov <artemdrolllted@gmail.com>
2025-07-31 17:12:13 +0300

    Initial Commit

---
+++ QuotesUI/QuotesUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesUIApp.swift
+//  QuotesUI
+//
+//  Created by Drolllted on 31.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuotesUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
